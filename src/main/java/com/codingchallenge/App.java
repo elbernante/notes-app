0bf3176575754c4b4b037791995eaf9c055d16eb
Peter James Bernante <elbernante@gmail.com>
2017-01-05 21:48:17 -0500

    feat: Implement backend API endpoints

--- src/main/java/com/codingchallenge/App.java
+++ src/main/java/com/codingchallenge/App.java
@@ -1,13 +1,78 @@
 package com.codingchallenge;
 
-/**
- * Hello world!
- *
- */
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import static spark.Spark.*;
+
+import com.codingchallenge.model.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	setPort(8888);
+    	staticFileLocation("/public");
+    	
+    	/**
+         * Creates new note.
+         *
+         * @param body        body of the note
+         */
+    	post("/api/notes", "application/json", (request, response) -> {
+        	Type type =  new TypeToken<Map<String, String>>() {}.getType();
+        	Map<String, String> data = new Gson().fromJson(request.body(), type);
+        	
+        	response.type("application/json");
+        	if (!data.containsKey("body")) {
+        		response.status(400); // Bad request
+        		return null;
+        	}
+        	
+        	response.status(201);      // Created
+        	return NotesDao.newNote(data.get("body"));
+        	
+        }, new JsonTransformer());
+        
+    	
+        /**
+         * Returns all a list of all notes.
+         */
+        get("/api/notes", "application/json", (request, response) -> {
+        	response.type("application/json");
+        	response.status(200);
+        	return NotesDao.allNotes(request.queryParams("query"));
+        }, new JsonTransformer());
+        
+        
+        /**
+         * Returns a note of a specified note id.
+         * 
+         * @param id	ID of of the note
+         */
+        get("/api/notes/:id", "application/json", (request, response) -> {
+        	String id = request.params("id");
+        	int idx;
+        	
+        	response.type("application/json");
+        	try {
+        		idx = Integer.valueOf(id);
+        	} catch (NumberFormatException e) {
+        		response.status(400);   // Bad request
+        		return null; 
+        	}
+        	
+        	Note note = NotesDao.getNote(idx);
+        	if (note == null) {
+        		response.status(404);  // Not found
+        		return null;
+        	}
+        	
+        	response.status(200);
+        	return note;
+        	
+        }, new JsonTransformer());
     }
 }
